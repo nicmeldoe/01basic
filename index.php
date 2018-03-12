3f6853022ad79caa720e0a8c2ceda32c42acb391
PR17 <PR17@figd-1711717.figdtk.de>
2018-03-12 14:17:10 +0100

    init project 01 basic

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>01 html basis</title>
+        <meta charset="UTF-8">
+        <title>PHP_basic01</title>
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="assets/css/styles.css" rel="stylesheet" type="text/css"/>
+        <script src="assets/js/jquery.min.js" type="text/javascript"></script>
+        <script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="assets/js/main.js" type="text/javascript"></script>
+    </head>
+    <body>
+	<h1>hghkjhldhf</h1>
+        <label>Name
+        <input class="form-control" type"text">
+        </label>
+        
+    <div class="row">
+        <div class="col-sm-12 col-md-6"> 
+            <h3>testtext 1</h3>blllblbllblblbllblblbfg f kkkkkkkkkkkkkk dkjk 
+            jnjkjd jkjjhjd kjjdhfjsdh kjhgjfhgdjkhsfjkhdjhlllbllllllllllllllllll
+            llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllbllblb
+            kblkblkblkvlöklcjkkjckljkj
+        </div>
+        <div class="col-sm-12 col-md-6">    
+            <h3>testtext 2</h3>blllblbllblblbllblblblllblllllllllllllllllllllll
+            lllllllllllllllllllllllllllllllllllllllllllllllllllllllllbllblbkblk
+            blkblkvlöklcjkkjckljkj
+        </div>
+    </div>        
+               </body>
+</html>
